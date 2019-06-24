25167638f9064411859cc1f2cb0d8e7e1969bb6f
MonsieurVuth <daravuth.pith@epsi.fr>
2019-06-24 15:52:26 +0200

    LE TRI

--- MapDistance/Program.cs
+++ MapDistance/Program.cs
@@ -35,16 +35,19 @@ namespace MapDistance
 
             //Calcul des distances
             Coordinate coor = new Coordinate();
-            List<String> listdistance = new List<String>();
+            Dictionary<String, String> listdistance = new Dictionary<String, String>();
+            List<String> cities = new List<string>();
             foreach (List<MapItem> ilist in list)
             {
+                String villes = "";
                 int index = ilist.Count;
                 Double distance = 0;
                 for (int j = 0; j < index; j++)
                 {
                     if (j < index - 1)
                     {
-                        distance += coor.GetDistance(ilist[j].lan, ilist[j].lng, ilist[j + 1].lan, ilist[j + 1].lng);                
+                        distance += coor.GetDistance(ilist[j].lan, ilist[j].lng, ilist[j + 1].lan, ilist[j + 1].lng);
+                        villes += ilist[j].city + "--";
                     }
                     else
                     {
@@ -52,21 +55,27 @@ namespace MapDistance
                     }
                 }
                 //Faire un dictionnaire key value (key = distance, value = list des villes)
-                listdistance.Add((distance / 1000).ToString());       
-               
+                listdistance.Add((distance / 1000).ToString(), villes);
             }
+
+            // Loop over pairs with foreach.
+            foreach (KeyValuePair<string, string> pair in listdistance.OrderBy(key => key.Key))
+            {
+                Console.WriteLine("{0}, {1}", pair.Key, pair.Value);
+            }
+        }
+
             //Trier par ordre croissant le dictionnaire et récupérer les XXX distances minimales et leur chemin associé
             //Faire un croisement entre ces chemins (exemple : prendre les 7 premiers d'un chemin et les 8 derniers d'un autre)
 
 
             ////Affichage des distances
-            var sortedlist = listdistance.OrderBy(d => d);
-            Console.WriteLine("Distance min : " + sortedlist.First());
+            //var sortedlist = listdistance.OrderBy(d => d);
+            //Console.WriteLine("Distance min : " + sortedlist.First());
             /*foreach (String dist in sortedlist)
             {
                 Console.WriteLine(dist);
             }*/
         }
     }
-   }
 
